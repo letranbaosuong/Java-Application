e91c1b4efce33f6ae91d1af8fc55f5027d1832b2
Le Tran Bao Suong <suong.66lab@gmail.com>
2019-09-30 08:33:54 +0700

    sửa cho nó giữa thôi á

--- Bai-Tap-Tuan/src/com/dhpm11/Tuan_4/Class/DemoFormBasic/src/com/dhpm11/demoformbasic/DemoFormBasic.java
+++ Bai-Tap-Tuan/src/com/dhpm11/Tuan_4/Class/DemoFormBasic/src/com/dhpm11/demoformbasic/DemoFormBasic.java
@@ -152,6 +152,7 @@ public class DemoFormBasic extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     /**
